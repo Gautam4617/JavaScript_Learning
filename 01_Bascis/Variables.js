0a611f02269e75dfeb8cc77e0d70fb02a772aa1b
Gautam4617 <gautamsharma4617@gmail.com>
2024-08-13 23:30:30 +0530

    ChangesDone

--- 01_Bascis/Variables.js
+++ 01_Bascis/Variables.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+const accountID = 1234
+let accountEmail = "gautamsharma4617@gmail.com"
+var accountPassword = "54321"
+accountCity = "Delhi"
+let accountState;
+
+// accountID = 1
+accountEmail = "Ballu @gmail.com "
+accountPassword = "121212"
+accountCity = "Haridwar"
+
+// console.log(accountID)
+
+/*
+Prefer not use var
+Because of issue in block scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
